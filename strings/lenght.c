1cbf3acd504a278f8d969ed596be3d63e32653eb
Jonathan Felicity <jonathanfelicity@mail.com>
2023-02-23 15:35:25 +0100

    finding lenght of a string done

---
+++ strings/lenght.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+
+
+int main(void){
+    int i;
+    char names[] = {'J', 'a', 'm', 'e', 's'};
+
+    for(i=0; names[i]!='\0'; i++ ){
+
+    }
+
+    printf("%d", i);
+
+    return 0;
+
+}
