32109d5a98bbeeda389a10cbf121bbfc4b0953c4
Jonathan Felicity <jonathanfelicity@mail.com>
2023-05-25 09:00:32 +0100

    add removing item to an array

--- array/static.c
+++ array/static.c
@@ -1,9 +1,40 @@
 #include <stdio.h>
 
+// removing item from an array
+// void remove(int arr[], int size, int index){
+
+//     return;
+// }
+
+
+// inserting item to array
+
+void insert(){
+
+    return;
+}
+
+void update(int arr[], int index, int value){
+
+    return;
+}
+
+
+
+int get(int arr[], int index){
+
+    return arr[index];
+}
+
+
 int main(void){
 
     int nums[] = {1, 2, 3 ,4 ,5 ,6, 7, 8};
-    printf("%d \n", nums[4]);
+
+    // modifying the element of the array
+    nums[0] = 10;
+
+    printf("%d \n", get(nums, 2));
 
     return 0;
 }
